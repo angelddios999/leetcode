be7b9d9ba2276a3fc008eed1b2b779bb9e6bfe98
Angel De Dios <angelddios999@gmail.com>
2019-04-13 20:09:21 -0700

    Updated the logic for AddTwoNumbers

--- src/main/java/com/angel/leetcode/AddTwoNumbers.java
+++ src/main/java/com/angel/leetcode/AddTwoNumbers.java
@@ -1,44 +1,30 @@
 package com.angel.leetcode;
 
-import java.math.BigInteger;
-
 public class AddTwoNumbers {
 
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        BigInteger number1 = listNodeToInt(l1);
-        BigInteger number2 = listNodeToInt(l2);
-        BigInteger sum = number1.add(number2);
-        return numberToNode(sum);
-    }
+        int l1Val = l1 != null ? l1.val : 0;
+        int l2Val = l2 != null ? l2.val : 0;
+        int sum = l1Val + l2Val;
+        boolean carriage = sum > 9;
+        ListNode node = new ListNode(sum % 10);
 
-    private BigInteger listNodeToInt(ListNode ln) {
-        StringBuilder sb = new StringBuilder();
-        ListNode node = ln;
-
-        while(node != null) {
-            sb.append(node.val);
-            node = node.next;
-        }
-        return new BigInteger(sb.reverse().toString());
-    }
-
-    private ListNode numberToNode(BigInteger number) {
-        String numberToString = new StringBuilder(String.valueOf(number)).reverse().toString();
-
-        int[] numbers = new int[numberToString.length()];
-        for(int i = 0; i < numberToString.length(); i++) {
-            numbers[i] = Integer.valueOf(Character.toString(numberToString.charAt(i)));
+        if(l1 != null && l1.next != null) {
+            if(carriage) {
+                l1.next.val++;
+            }
+            node.next = addTwoNumbers(l1.next, l2 != null ? l2.next : null);
+        } else if(l2 != null && l2.next != null) {
+            if(carriage) {
+                l2.next.val++;
+            }
+            node.next = addTwoNumbers(l1 != null ? l1.next : null, l2.next);
+        } else if(carriage){
+            ListNode carriageNode = new ListNode(1);
+            node.next = carriageNode;
         }
 
-        ListNode listNode = new ListNode(numbers[0]);
-
-        ListNode tempNode = listNode;
-        for(int i = 1; i < numbers.length; i++) {
-            tempNode.next = new ListNode(numbers[i]);
-            tempNode = tempNode.next;
-        }
-
-        return  listNode;
+        return node;
     }
 }
 
