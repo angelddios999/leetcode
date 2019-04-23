b56ae9981e7a37a849ff796bdd618449a227a4bf
Angel De Dios <angelddios999@gmail.com>
2019-04-22 23:34:26 -0700

    Improved the logic for ReverseInteger
    Signed-off-by: Angel De Dios <angelddios999@gmail.com>

--- src/main/java/com/angel/leetcode/ReverseInteger.java
+++ src/main/java/com/angel/leetcode/ReverseInteger.java
@@ -2,16 +2,28 @@ package com.angel.leetcode;
 
 public class ReverseInteger {
     public int reverse(int x) {
-        long reversed = 0;
-        while(x != 0) {
-            reversed = reversed * 10 + x % 10;
-            x /= 10;
+        if (Math.abs(x / 10) > 0) {
+            int reversed = x % 10;
+            x = x / 10;
+
+            while (Math.abs(x) > 0) {
+                int mod = x % 10;
+                if (inBounds(reversed * 10L + mod)) {
+                    reversed *= 10;
+                    reversed += mod;
+                    x = x / 10;
+                } else {
+                    return 0;
+                }
+            }
+
+            return reversed;
         }
 
-        if(reversed > Integer.MAX_VALUE || reversed < Integer.MIN_VALUE) {
-            return 0;
-        }
+        return x;
+    }
 
-        return (int)reversed;
+    private boolean inBounds(long value) {
+        return value < Integer.MAX_VALUE && value > Integer.MIN_VALUE;
     }
 }
