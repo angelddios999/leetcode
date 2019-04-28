ea66934b13db657eb2a09eda18f92b05716938a2
Angel De Dios <angelddios999@gmail.com>
2019-04-27 17:46:39 -0700

    Completed StringToInteger

--- src/main/java/com/angel/leetcode/StringToInteger.java
+++ src/main/java/com/angel/leetcode/StringToInteger.java
@@ -1,4 +1,53 @@
 package com.angel.leetcode;
 
 public class StringToInteger {
+    public int myAtoi(String str) {
+        char[] input = str.toCharArray();
+        boolean negative = false;
+        boolean numberStarted = false;
+        int result = 0;
+
+        for (int i = 0; i < input.length; i++) {
+            char c = input[i];
+            if (c == ' ') {
+                // Spaces
+                if (numberStarted) {
+                    break;
+                } else {
+                    continue;
+                }
+            } else if (c <= '9' && c >= '0') {
+                // Numbers
+                int value = c - '0';
+                if (inBounds((result * 10L) + value)) {
+                    result = (result * 10) + value;
+                    numberStarted = true;
+                } else {
+                    return negative ? Integer.MIN_VALUE : Integer.MAX_VALUE;
+                }
+            } else if (c  == '-' || c == '+') {
+                // Signs
+                if (numberStarted) {
+                    break;
+                }
+                if (c  == '-') {
+                    negative = true;
+                }
+                numberStarted = true;
+            } else {
+                // Any other character
+                if (numberStarted) {
+                    break;
+                } else {
+                    return 0;
+                }
+            }
+        }
+
+        return negative ? result * -1 : result;
+    }
+
+    private boolean inBounds(long value) {
+        return value <= Integer.MAX_VALUE && value >= Integer.MIN_VALUE;
+    }
 }
--- src/test/java/com/angel/leetcode/StringToIntegerTest.java
+++ src/test/java/com/angel/leetcode/StringToIntegerTest.java
@@ -1,4 +1,42 @@
 package com.angel.leetcode;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class StringToIntegerTest {
+    @Test
+    public void testMyAtoi1() {
+        StringToInteger stoi = new StringToInteger();
+        Assert.assertEquals(42, stoi.myAtoi("42"));
+    }
+    @Test
+    public void testMyAtoi2() {
+        StringToInteger stoi = new StringToInteger();
+        Assert.assertEquals(-42, stoi.myAtoi("-42"));
+    }
+    @Test
+    public void testMyAtoi3() {
+        StringToInteger stoi = new StringToInteger();
+        Assert.assertEquals(4193, stoi.myAtoi("4193 with words"));
+    }
+    @Test
+    public void testMyAtoi4() {
+        StringToInteger stoi = new StringToInteger();
+        Assert.assertEquals(0, stoi.myAtoi("words and 987"));
+    }
+    @Test
+    public void testMyAtoi5() {
+        StringToInteger stoi = new StringToInteger();
+        Assert.assertEquals(-2147483648, stoi.myAtoi("-91283472332"));
+    }
+    @Test
+    public void testMyAtoi6() {
+        StringToInteger stoi = new StringToInteger();
+        Assert.assertEquals(-2147483647, stoi.myAtoi("-2147483647"));
+    }
+    @Test
+    public void testMyAtoi7() {
+        StringToInteger stoi = new StringToInteger();
+        Assert.assertEquals(-5, stoi.myAtoi("-5-"));
+    }
 }
