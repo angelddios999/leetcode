6c90de8c45fe80953e177e990e4a9a01d25a7240
Angel De Dios <angelddios999@gmail.com>
2019-05-15 23:52:16 -0700

    RegExMatching completed but failing one test case

--- src/main/java/com/angel/leetcode/RegExMatching.java
+++ src/main/java/com/angel/leetcode/RegExMatching.java
@@ -1,4 +1,74 @@
 package com.angel.leetcode;
 
+import java.util.Arrays;
+
 public class RegExMatching {
+    public boolean isMatch(String s, String p) {
+        char[][] tokens = getTokens(p);
+        char[] word = s.toCharArray();
+        int tokensIndex = 0;
+        int wordIndex = 0;
+        char lastWildcard = '\u0000';
+        char lastCharacter = '\u0000';
+
+        while (wordIndex < word.length) {
+            // If the regex ended but the word contains more characters
+            if (tokensIndex >= tokens.length) {
+                return false;
+            }
+            char c = word[wordIndex];
+            char t = tokens[tokensIndex][0];
+            boolean isWildcard = tokens[tokensIndex][1] == '*';
+
+            if (t == c || t == '.') {
+                if (!isWildcard) {
+                    tokensIndex++;
+                }
+                wordIndex++;
+            } else {
+                if (isWildcard) {
+                    tokensIndex++;
+                } else {
+                    return false;
+                }
+            }
+
+            if (isWildcard) {
+                lastWildcard = t;
+            }
+
+            lastCharacter = c;
+        }
+
+        // If there are tokens left
+        for (int i = tokensIndex; i < tokens.length; i++) {
+            char t = tokens[i][0];
+            boolean isWildcard = tokens[i][1] == '*';
+
+            // False if the token is not wildcard AND doesn't match the last character or there were no wildcards
+            if (!isWildcard) {
+                if ((t == '.' || t != lastWildcard) && t != lastCharacter || lastWildcard == '\u0000') {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    private char[][] getTokens(String p) {
+        char[] regex = p.toCharArray();
+        char[][] tokens = new char[p.length()][2];
+        int tokensIndex = 0;
+        for (int i = 0; i < regex.length; i++) {
+            tokens[tokensIndex][0] = regex[i];
+            if (i < regex.length - 1 && regex[i + 1] == '*') {
+                tokens[tokensIndex][1] = '*';
+                i++;
+            }
+            tokensIndex++;
+        }
+        tokens = Arrays.copyOf(tokens, tokensIndex);
+        return tokens;
+    }
 }
--- src/test/java/com/angel/leetcode/RegExMatchingTest.java
+++ src/test/java/com/angel/leetcode/RegExMatchingTest.java
@@ -1,4 +1,79 @@
 package com.angel.leetcode;
 
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
 public class RegExMatchingTest {
+    @Test
+    public void testRegExMatching1() {
+        RegExMatching rxm = new RegExMatching();
+        assertThat(rxm.isMatch("aa", "a"), is(false));
+    }
+    @Test
+    public void testRegExMatching2() {
+        RegExMatching rxm = new RegExMatching();
+        assertThat(rxm.isMatch("aa", "a*"), is(true));
+    }
+    @Test
+    public void testRegExMatching3() {
+        RegExMatching rxm = new RegExMatching();
+        assertThat(rxm.isMatch("ab", ".*"), is(true));
+    }
+    @Test
+    public void testRegExMatching4() {
+        RegExMatching rxm = new RegExMatching();
+        assertThat(rxm.isMatch("aab", "c*a*b"), is(true));
+    }
+    @Test
+    public void testRegExMatching5() {
+        RegExMatching rxm = new RegExMatching();
+        assertThat(rxm.isMatch("mississippi", "mis*is*p*."), is(false));
+    }
+    @Test
+    public void testRegExMatching6() {
+        RegExMatching rxm = new RegExMatching();
+        assertThat(rxm.isMatch("ab", ".*c"), is(false));
+    }
+    @Test
+    public void testRegExMatching7() {
+        RegExMatching rxm = new RegExMatching();
+        assertThat(rxm.isMatch("aaa", "a*a"), is(true));
+    }
+    @Test
+    public void testRegExMatching8() {
+        RegExMatching rxm = new RegExMatching();
+        assertThat(rxm.isMatch("", ".*"), is(true));
+    }
+    @Test
+    public void testRegExMatching9() {
+        RegExMatching rxm = new RegExMatching();
+        assertThat(rxm.isMatch("aaa", "aaaa"), is(false));
+    }
+    @Test
+    public void testRegExMatching10() {
+        RegExMatching rxm = new RegExMatching();
+        assertThat(rxm.isMatch("aaa", "ab*a*c*a"), is(true));
+    }
+    @Test
+    public void testRegExMatching11() {
+        RegExMatching rxm = new RegExMatching();
+        assertThat(rxm.isMatch("a", ".*..a*"), is(false));
+    }
+    @Test
+    public void testRegExMatching12() {
+        RegExMatching rxm = new RegExMatching();
+        assertThat(rxm.isMatch("bbbba", ".*a*a"), is(true));
+    }
+    @Test
+    public void testRegExMatching13() {
+        RegExMatching rxm = new RegExMatching();
+        assertThat(rxm.isMatch("aaaaaaaab", "a*b"), is(true));
+    }
+    @Test
+    public void testRegExMatching14() {
+        RegExMatching rxm = new RegExMatching();
+        assertThat(rxm.isMatch("ab", ".*.."), is(true));
+    }
 }
